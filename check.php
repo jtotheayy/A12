1a4d30014b9a84d82e1fdde3eb8c0edbe447f84a
Jay Santamaria <102671405+jtotheayy@users.noreply.github.com>
2022-11-22 13:55:03 -0500

    Upload files

---
+++ check.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>check cookie</title>
+</head>
+
+<?php
+session_start();
+
+if(!isset($_SESSION["cookie"])){
+	header("Location: A12.php");
+	die;
+}
+$cookie = $_SESSION["cookie"];
+
+echo $cookie;
+?>
+<body>
+    <form method="post" action="A12.php">
+        <input type="submit" value="HOME" />
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>A12 Cookies Jay Santamaria</title>
+</head>
+
+<body>
+    <form method="post">
+        <label for="cookie"><strong>Enter a data for a Cookie:</strong></label>
+        <input type="text" id="cookie" name="cookie" />
+        <br />
+        <input type="submit" value="Submit" />
+    </form>
+</body>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+session_start();
+
+$cookie = $_POST['cookie'];
+$_SESSION["cookie"] = $cookie;
+
+
+if (isset($cookie)) {
+	header("Location:check.php");
+}
+
+}
+?>
+</html>
